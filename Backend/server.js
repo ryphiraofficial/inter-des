057bf1c44b2bec067c81dc7741d269f6015ae2f6
rohith-sunilkumar <rohith.sunilkumar09@gmail.com>
2026-06-04 15:20:34 +0530

    fix(dotenv): load env variables before other es module imports

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,9 @@
+import './env.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-import dotenv from 'dotenv';
-dotenv.config({ path: path.join(__dirname, '.env') });
 
 import express from 'express';
 import mongoose from 'mongoose';
